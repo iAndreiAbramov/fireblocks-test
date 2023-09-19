38149b64154851f623401a3c60ce34ac92182195
Andrey_Abramov <i.andrei.abramov@gmail.com>
2023-09-19 14:28:32 +0300

    feat: updates app-config with app mode

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,16 +2,19 @@ import { registerAs } from '@nestjs/config';
 import { IAppConfig } from '@src/types/app-config.interface';
 import * as Joi from 'joi';
 import * as process from 'process';
+import { TAppMode } from '@src/types/app-mode.type';
 
 export default registerAs('app', (): IAppConfig => {
   const config: IAppConfig = {
     port: parseInt(process.env.PORT),
+    mode: process.env.MODE as TAppMode | undefined,
     apiKey: process.env.API_KEY,
     secretKey: process.env.SECRET_KEY,
   };
 
   const schema = Joi.object<IAppConfig, null, IAppConfig>({
     port: Joi.number().port().required(),
+    mode: Joi.string().required().valid('dev', 'prod'),
     apiKey: Joi.string().required(),
     secretKey: Joi.string().required(),
   });
--- src/types/app-config.interface.ts
+++ src/types/app-config.interface.ts
@@ -1,5 +1,8 @@
+import { TAppMode } from '@src/types/app-mode.type';
+
 export interface IAppConfig {
   port: number;
+  mode: TAppMode;
   apiKey: string;
   secretKey: string;
 }
